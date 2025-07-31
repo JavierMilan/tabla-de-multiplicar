ab388c3bf52d58d0405a2ea5f99a593ef7a908bd
JavierMilan <jamilanfe@gmail.com>
2025-07-31 17:55:07 +0200

    añadiendo

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 function multiplicar(){
-    const numeroUsuario = Number(document.getElementById("numeroUsuario").value);
-
-    let resultado = "";
-
-    for (let i = 1; i <= 10; i++) {
-        resultado += `${numeroUsuario} x ${i} = ${numeroUsuario * i}\n`;
-    }
-
-    document.getElementById("resultado").textContent = resultado;
+const numeroUsuario = Number(document.getElementById("numeroUsuario").value);
+let resultadoFinal = "";
+for(let i = 1 ; i <= 10 ; i++){
+resultadoFinal += `${numeroUsuario} x ${i} = ${numeroUsuario * i}\n`; 
 }
- 
+document.getElementById("resultado").textContent = resultadoFinal;
+}
